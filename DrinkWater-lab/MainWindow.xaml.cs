bc803d1f439b7d9b816f16dcb6c9a8091c8a28e3
Emma Svensson <emsv2000@student.miun.se>
2025-11-10 11:46:55 +0100

    Drink water steg 1

--- DrinkWater-lab/MainWindow.xaml.cs
+++ DrinkWater-lab/MainWindow.xaml.cs
@@ -14,7 +14,19 @@ namespace DrinkWater_lab
 
         private void OnButtonCalculateClick(object sender, RoutedEventArgs e)
         {
+            if (double.TryParse(txtHours.Text, out double hours))
+            {
+                double liters = hours * 0.5;
+                int roundedLiters = (int)Math.Floor(liters);
 
+                MessageBox.Show($"Du bör dricka {roundedLiters} liter vatten.");
+            }
+            else
+            {
+                MessageBox.Show("Felaktig inmatning. Skriv in ett giltigt tal för antal timmar.");
+            }
         }
     }
 }
+//källor
+//https://learn.microsoft.com/en-us/dotnet/api/system.math.floor
